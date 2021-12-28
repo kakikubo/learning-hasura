bfc37fe08459f64c06ea488b1f9090e3ffd74588
kakikubo <kakikubo@gmail.com>
2021-12-29 04:47:08 +0900

    tailwind.cssのpurge設定追加

--- kakikubo-hasura/tailwind.config.js
+++ kakikubo-hasura/tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  darkMode: false,
   content: [],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
