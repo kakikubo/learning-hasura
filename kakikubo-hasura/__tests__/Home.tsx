792770658a74289a5c0a17d08d051a10a8a06b69
kakikubo <kakikubo@gmail.com>
2022-01-04 06:47:49 +0900

    テストが通るように調整

--- kakikubo-hasura/__tests__/Home.tsx
+++ kakikubo-hasura/__tests__/Home.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import Home from '../pages/index'
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Home from '../pages/index';
 
 it('Should render title text', () => {
-  render(<Home />)
-  expect(screen.getByText('Next.js!')).toBeInTheDocument()
-})
+  render(<Home />);
+  expect(screen.getByText('Next.js + GraphQL')).toBeInTheDocument();
+});
