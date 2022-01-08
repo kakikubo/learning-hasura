5b227c050a0bcdbb00d99210bfdec578ed7559b2
kakikubo <kakikubo@gmail.com>
2022-01-09 07:24:46 +0900

    そもそも書き方間違っていた件

--- kakikubo-hasura/pages/_app.tsx
+++ kakikubo-hasura/pages/_app.tsx
@@ -3,11 +3,11 @@ import { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
 import { initializeApollo } from '../lib/apolloClient';
 
-function MyApp({ Component, pageProps }): AppProps {
+function MyApp({ Component, pageProps }: AppProps) {
   const client = initializeApollo();
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ApolloProvider>
   );
 }
