4f2d3d97f2b8cc92278a60e78390046251bc05af
kakikubo <kakikubo@gmail.com>
2022-01-11 07:19:11 +0900

    テストデータとHasuraMainとHasuraCRUDのページの表示要素テスト

---
+++ kakikubo-hasura/__tests__/HasuraMain.test.tsx
@@ -0,0 +1,38 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
+import { getPage, initTestHelpers } from 'next-page-tester';
+import { setupServer } from 'msw/node';
+import { handlers } from '../mock/handlers';
+import 'setimmediate';
+
+initTestHelpers();
+
+const server = setupServer(...handlers);
+
+beforeAll(() => {
+  server.listen();
+});
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+afterAll(() => {
+  server.close();
+});
+
+describe('Hasura Fetch Test Cases', () => {
+  it('Shoud render the list of users by useQuery', async () => {
+    const { page } = await getPage({
+      route: '/hasura-main',
+    });
+    render(page);
+    expect(await screen.findByText('Hasura main page')).toBeInTheDocument();
+    expect(await screen.findByText('test user1')).toBeInTheDocument();
+    expect(screen.getByText('test user2')).toBeInTheDocument();
+    expect(screen.getByText('test user3')).toBeInTheDocument();
+  });
+});
--- kakikubo-hasura/mock/handlers.ts
+++ kakikubo-hasura/mock/handlers.ts
@@ -7,19 +7,19 @@ export const handlers = [
           {
             __typename: 'users',
             id: '9f5d5799-fc4f-4472-a2a3-0d4bedce6e70',
-            name: 'user1',
+            name: 'test user1',
             created_at: '2021-01-24T21:58:35.54389+00:00',
           },
           {
             __typename: 'users',
             id: 'c06d9066-d2bc-4502-9e79-380f801ec74d',
-            name: 'user2',
+            name: 'test user2',
             created_at: '2021-02-24T21:58:43.443901+00:00',
           },
           {
             __typename: 'users',
             id: 'ec1a5512-9f49-442c-9bc3-bbe5de91dbb9',
-            name: 'user3',
+            name: 'test user3',
             created_at: '2021-03-24T21:59:20.423826+00:00',
           },
         ],
@@ -54,7 +54,7 @@ export const handlers = [
           users_by_pk: {
             __typename: 'users',
             id: '9f5d5799-fc4f-4472-a2a3-0d4bedce6e70',
-            name: 'user1',
+            name: 'test user1',
             created_at: '2021-01-24T21:58:35.54389+00:00',
           },
         })
@@ -66,7 +66,7 @@ export const handlers = [
           users_by_pk: {
             __typename: 'users',
             id: 'c06d9066-d2bc-4502-9e79-380f801ec74d',
-            name: 'user2',
+            name: 'test user2',
             created_at: '2021-02-24T21:58:43.443901+00:00',
           },
         })
@@ -78,7 +78,7 @@ export const handlers = [
           users_by_pk: {
             __typename: 'users',
             id: 'ec1a5512-9f49-442c-9bc3-bbe5de91dbb9',
-            name: 'user3',
+            name: 'test user3',
             created_at: '2021-03-24T21:59:20.423826+00:00',
           },
         })
