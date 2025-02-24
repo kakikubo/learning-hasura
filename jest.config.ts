c5fec78cf66d62d6e07cf4903ca0eb080fa2ca2b
kakikubo <kakikubo@gmail.com>
2025-02-24 18:04:38 +0900

    試行錯誤中。ただ、エラー解消はされてない
    % pnpm test __tests__/HasuraSSG.test.tsx
    
    > kakikubo-hasura@ test /Users/teruo.kakikubo/Documents/kakikubo/learning-hasura
    > NODE_ENV=test jest --config jest.config.ts --env=jsdom --verbose __tests__/HasuraSSG.test.tsx
    
     FAIL  __tests__/HasuraSSG.test.tsx
      ● Test suite failed to run
    
        ReferenceError: Response is not defined
    
           5 | import @testing-library/jest-dom;
           6 | import { http, HttpResponse } from msw;
        >  7 | import { setupServer } from msw/node; // msw/nodeを使用
             | ^
           8 | import { handlers } from ../mock/handlers;
           9 | import setimmediate;
          10 | import HasuraSSG from ../pages/hasura-ssg;
    
          at Object.<anonymous> (node_modules/.pnpm/@mswjs+interceptors@0.37.6/node_modules/@mswjs/interceptors/src/utils/fetchUtils.ts:5:8)
          at Object.<anonymous> (node_modules/.pnpm/@mswjs+interceptors@0.37.6/node_modules/@mswjs/interceptors/lib/node/chunk-3HCE66HZ.js:13:24)
          at Object.<anonymous> (node_modules/.pnpm/@mswjs+interceptors@0.37.6/node_modules/@mswjs/interceptors/lib/node/interceptors/ClientRequest/index.js:3:24)
          at Object.<anonymous> (node_modules/.pnpm/msw@2.7.1_@types+node@22.13.5_typescript@5.7.3/node_modules/msw/src/node/SetupServerApi.ts:2:42)
          at Object.<anonymous> (__tests__/HasuraSSG.test.tsx:7:1)
    
    Test Suites: 1 failed, 1 total
    Tests:       0 total
    Snapshots:   0 total
    Time:        2.307 s
    Ran all test suites matching /__tests__\/HasuraSSG.test.tsx/i.
     ELIFECYCLE  Test failed. See above for more details.
    `
    `

--- jest.config.ts
+++ jest.config.ts
@@ -3,14 +3,22 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    customExportConditions: [''],
+  },
+  rootDir: __dirname,
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
     '\\.(css)$': '<rootDir>/node_modules/jest-css-modules',
+    '^msw$': '<rootDir>/node_modules/msw/lib/index.js',
+    '^@mswjs$': '<rootDir>/node_modules/@mswjs',
   },
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!msw|@mswjs)'],
+  modulePaths: ['<rootDir>/src'],
 };
 
 export default config;
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,8 @@
 // jest.setup.ts
 import '@testing-library/jest-dom';
+import { fetch, Request, Response, Headers } from 'cross-fetch';
+
+global.fetch = fetch;
+global.Request = Request;
+global.Response = Response;
+global.Headers = Headers;
